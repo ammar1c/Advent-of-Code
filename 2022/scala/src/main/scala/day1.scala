50db92e7a36512945432d3994b72a2de8eda48e4
Ammar Al-Batool <ammara@spotify.com>
2022-12-01 13:32:09 +0000

    day1 2022

---
+++ 2022/scala/src/main/scala/day1.scala
@@ -0,0 +1,19 @@
+import scala.collection.mutable
+
+object day1 {
+
+  def main(args: Array[String]): Unit = {
+    val file = getClass.getResource("/day1.txt").getFile
+    val lines = io.Source.fromFile(file).getLines()
+    var sums = mutable.ArrayDeque.empty[Long]
+    for(line <- lines) {
+      if(sums.isEmpty) sums += 0
+
+      if (line.isEmpty) sums += 0
+      else  sums.update(sums.size-1, sums.last+line.toLong)
+    }
+    println(sums.max)
+    println(sums.sortBy(-_).take(3).sum)
+
+  }
+}
